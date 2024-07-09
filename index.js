1e322dae6848b198390081083a430c1502d593bb
Beftli <77486662+beftlidev@users.noreply.github.com>
2024-07-10 00:45:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const axios = require("axios").default;
 const { load } = require("cheerio").default;
 const puppeteer = require('puppeteer');
+const { exec } = require('child_process');
+const package = require("./package.json");
+
+async function checkUpdate() {
+
+    function getPackageVersion() {
+
+        return new Promise((resolve, reject) => {
+            exec(`npm view streamer.info version`, (error, stdout, stderr) => {
+                if (error) return { error };
+                if (stderr) return { error: stderr };
+                resolve({ version: stdout.trim(), error: false });
+            });
+        });
+        
+    }
+
+    const version = await getPackageVersion()
+
+    if(version.error) return;
+
+    if(version.version) {
+        if(version.version !== package.version) {
+            console.log(`\x1b[34mStreamer.Info is out of date!\x1b[0m Remember to use \x1b[32m"npm update streamer.info" ( ${package.version} -> ${version.version} )\x1b[0m in powershell to take advantage of the new features.`)
+        } else return;
+    } else return;
+
+}
 
 class Kick {
     
     async getStream(username) {
         
         let browser;
+
+        if(!username) return { live: false, error: "You have to enter a streamer name." }
     
         try {
 
@@ -15,10 +45,8 @@ class Kick {
             });
     
             const page = await browser.newPage();
-
-            const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
             
-            await page.setUserAgent(userAgent);
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
     
             const response = await page.goto(`https://kick.com/api/v2/channels/${username}/livestream`, {
                 waitUntil: 'networkidle2'
@@ -103,12 +131,93 @@ class Kick {
         }
       
     }
+
+    async getStreamerProfile(username) {
+        
+        let browser;
+
+        if(!username) return { live: false, error: "You have to enter a streamer name." }
+    
+        try {
+
+            browser = await puppeteer.launch({
+                headless: 'new'
+            });
+    
+            const page = await browser.newPage();
+            
+            await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+    
+            const response = await page.goto(`https://kick.com/api/v2/channels/${username}`, {
+                waitUntil: 'networkidle2'
+            });
+    
+            if (response && response.ok()) {
+    
+                const data = await page.evaluate(() => document.body.textContent);
+            
+                const info = JSON.parse(data)
+          
+                if(info == null) return { live: false, error: false }
+            
+                const { verified } = info;
+
+                const { profile_pic, bio, instagram, twitter, youtube, discord, tiktok, facebook } = info.user;
+            
+                return {
+                    success: true,
+                    error: false,
+                    avatar: profile_pic,
+                    bio,
+                    verified, 
+                    socials: {
+                        instagram: {
+                            row: instagram ? instagram : false,
+                            link: instagram ? `https://instagram.com/${instagram}` : false
+                        }, 
+                        x: {
+                            row: twitter ? twitter : false,
+                            link: twitter ? `https://x.com/${twitter}` : false
+                        }, 
+                        youtube: {
+                            row: youtube ? youtube : false,
+                            link: youtube ? `https://youtube.com/${youtube}` : false
+                        }, 
+                        discord: {
+                            row: discord ? discord : false
+                        }, 
+                        tiktok: {
+                            row: tiktok ? tiktok : false,
+                            link: tiktok ? `https://tiktok.com/${tiktok}` : false
+                        }, 
+                        facebook: {
+                            row: facebook ? facebook : false,
+                            link: facebook ? `https://facebook.com/${facebook}` : false
+                        }
+                    }
+                };
+        
+            } else {
+                return { success: false, error: `Failed to load URL: ${response?.status()} - ${response?.statusText()}` }
+            }
+
+        } catch (error) {
+            return { success: false, error }
+        } finally {
+            if (browser) {
+                await browser.close();
+            }
+        }
+      
+    }
     
 }
 
 class Twitch {
     
     async getStream (username) {
+
+        if(!username) return { live: false, error: "You have to enter a streamer name." }
     
         const { data } = await axios(`https://www.twitch.tv/${encodeURIComponent(username)}`);
     
@@ -132,6 +241,7 @@ class Twitch {
             const json = JSON.parse(scriptTagContent);
     
             const { name, description, embedUrl, thumbnailUrl, publication: { startDate } } = json["@graph"][0];
+            const profilePhoto = $('meta[property="og:image"]').attr('content')
 
             let thumbnail;
             if (thumbnailUrl.length > 0) {
@@ -167,6 +277,7 @@ class Twitch {
                 live: true,
                 error: false,
                 name,
+                avatar: profilePhoto,
                 title: description,
                 thumbnail: {
                     src: thumbnailUrl ? thumbnailUrl : false,
@@ -220,6 +331,7 @@ class Twitch {
 }
   
 module.exports = {
+    checkUpdate,
     Kick,
     Twitch
 }
